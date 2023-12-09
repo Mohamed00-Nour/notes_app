6f2da08d39a42d1d9e51a0dd2ea8bd5c7b93a54e
Mohamed00-Nour <mk3758131@gmail.com>
2023-12-09 17:32:34 +0200

    Create fetch note method

--- lib/Cubits/notes_list/notes_cubit.dart
+++ lib/Cubits/notes_list/notes_cubit.dart
@@ -1,10 +1,20 @@
 import 'package:bloc/bloc.dart';
-import 'package:meta/meta.dart';
-
+import 'package:hive/hive.dart';
 import '../../Models/Note_Model.dart';
+import '../../constants.dart';
 
 part 'notes_state.dart';
 
 class NotesCubit extends Cubit<NotesState> {
   NotesCubit() : super(NotesInitial());
+
+  fetchNotes() async {
+    emit(NotesLoading());
+    try {
+      List<NoteModel> notesList =  Hive.box<NoteModel>(kNotesBox).values.toList();
+      emit(NotesSuccess(notesList));
+    } catch (e) {
+      emit(NotesFailure(e.toString()));
+    }
+  }
 }
--- lib/Views/notes_view.dart
+++ lib/Views/notes_view.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:my_notes/Cubits/notes_list/notes_cubit.dart';
 
 import '../Widgets/Add_Note_BottomSheet.dart';
 import '../Widgets/Notes_View_Body.dart';
@@ -8,21 +10,25 @@ class NotesView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return  Scaffold(
-      floatingActionButton: FloatingActionButton(
-        onPressed: (){
-          showModalBottomSheet(
-            isScrollControlled: true,
-            context: context,
-            builder: (context) {
-              return const AddNoteBottomSheet();
-            },
-          );
-        },
-        child: const Icon(Icons.add),
-      ),
-      body: const NotesViewBody(),
+    return BlocProvider(
+      create: (context) => NotesCubit(),
+      child: Scaffold(
+        resizeToAvoidBottomInset: false,
+        floatingActionButton: FloatingActionButton(
+          onPressed: () {
+            showModalBottomSheet(
+              isScrollControlled: true,
+              context: context,
+              builder: (context) {
+                return const AddNoteBottomSheet();
+              },
+            );
+          },
+          child: const Icon(Icons.add),
+        ),
+        body: const NotesViewBody(),
 
+      ),
     );
   }
 }
